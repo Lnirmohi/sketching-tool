1c85dad0fb0ae77b1d962d157c51410773f06477
Ubuntu <tester@techpearl.com>
2022-10-09 20:10:48 +0530

    refactor mouse event on graphics to separate functions

--- src/pages/sketch-home/sketch-home.js
+++ src/pages/sketch-home/sketch-home.js
@@ -1,89 +1,132 @@
-import { useRef, useEffect, useCallback, useState } from "react";
+import { useRef, useEffect, useCallback, useState, useContext } from "react";
 import { Stage, Graphics } from "@saitonakamura/react-pixi";
 import * as PIXI from "pixi.js";
 
+import "./sketch-home.css";
+import { AuthContext } from "../../services/authenticate";
+
 function SketchHome() {
+
+    const stageRef = useRef();
+    const auth = useContext(AuthContext);
 
     const lastPos = {x: 0, y: 0};
     let isDrawing = false;
 
-    const draw = useCallback((graphics) => {
+    const draw = (graphics) => {
         console.log(graphics);
 
-
         graphics.beginFill(0xFFFFFF, 1)
-        graphics.drawRect(0, 0, window.innerWidth, window.innerWidth)
+        graphics.drawRect(0, 0, 1600, 800)
         graphics.endFill();
 
-        graphics.on("mousedown", (e) => {
+        graphics.on("mousedown", mdown);
+        graphics.on("mouseup",  mup);
+        graphics.on("mousemove", mmove);
+        graphics.on("mouseout", mout);
+        
+        console.log(stageRef);
+        console.log(auth);
+    };
+
+    const mout = (e) => {
+        isDrawing = false;
+    };
+
+    const mdown = (e) => {
             
-            if(isDrawing) {
-                return;
-            } 
-            const {x, y} = e.data.global;
+        if(isDrawing) {
+            return;
+        } 
+        const {x, y} = e.data.global;
+        
+        lastPos.x = x;
+        lastPos.y = y;
+        
+        isDrawing = true;
+    };
+
+    const mup = (e) => {
+        isDrawing = false;
+        console.log(stageRef, e);
+        saveSketchAsImage(e.target);
+    };
+
+    const mmove = (e) => {
+
+        if(!isDrawing) {
+            return;
+        }
+
+        const {x, y} = e.data.global;
+        const target = e.target;
+        target.lineStyle({
+            width: 10,
+            cap: PIXI.LINE_CAP.BUTT,
+            join: PIXI.LINE_JOIN.BEVEL,
+            color: 0xFBE0,
+            native: true
             
-            lastPos.x = x;
-            lastPos.y = y;
-            
-            isDrawing = true;
         });
 
-        graphics.on("mouseup",  (e) => {
-            isDrawing = false;
+        const currentPosition = {};
+
+        currentPosition.x = x;
+        currentPosition.y = y;
+
+        target.beginFill(0xffffff, 1);
+        target.moveTo(lastPos.x, lastPos.y);
+        target.lineTo(currentPosition.x, currentPosition.y);
+        target.endFill();
+        
+        lastPos.x = currentPosition.x;
+        lastPos.y = currentPosition.y;
+
+    };
+
+    const saveSketchAsImage = (graphicsObj) => {
+
+        const app = stageRef.current.app;
+
+        app.renderer.plugins.extract.canvas(graphicsObj).toBlob((blob) => {
             
-        });
-
-        graphics.on("mousemove", (e) => {
-
-            if(!isDrawing) {
-                return;
-            }
-
-            const {x, y} = e.data.global;
-            const target = e.target;
-            target.lineStyle({
-                width: 5,
-                cap: PIXI.LINE_CAP.BUTT,
-                join: PIXI.LINE_JOIN.BEVEL,
-                color: 0xFBE0,
-                native: true
-                
-            });
-
-            const currentPosition = {};
-
-            currentPosition.x = x;
-            currentPosition.y = y;
-
-            target.beginFill(0xffffff, 1);
-            target.moveTo(lastPos.x, lastPos.y);
-            target.lineTo(currentPosition.x, currentPosition.y);
-            // target.endFill();
-            
-            lastPos.x = currentPosition.x;
-            lastPos.y = currentPosition.y;
-            
-        });
-
-    }, []);
+            const newImg = document.createElement('img');
+            const url = URL.createObjectURL(blob);
+          
+            newImg.onload = () => {
+              // no longer need to read the blob so it's revoked
+              URL.revokeObjectURL(url);
+            };
+          
+            newImg.src = url;
+            console.log(newImg);
+            // document.body.appendChild(newImg);
+          });
+    };
 
     return (
-        <div id="sketch-container">
-            <h1>Sketch Homes</h1>
-            <Stage
-                width={window.innerWidth} 
-                height={window.innerHeight} 
-                options={{ 
-                    backgroundColor: 0xffffff, 
-                    antialias: true,
-                    interactive: true
-                }}
-            >
-                <Graphics 
-                    draw={draw}
-                    interactive={true}
-                />
-            </Stage>
+        <div className="sketch-container">
+            <div className="sketch-header">
+                <h1>Sketch Home</h1>
+                <p className="user">{auth.auth.username}</p>
+            </div>
+            <div className="stage-container">
+                <Stage
+                    width={1600}
+                    height={800}
+                    options={{
+                        backgroundColor: 0xffffff,
+                        antialias: true,
+                        interactive: true
+                    }}
+                    ref={stageRef}
+                >
+                    <Graphics
+                        draw={draw}
+                        interactive={true}
+                    />
+                </Stage>
+            </div>
         </div>
     );
 }
