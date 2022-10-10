d5c7279f2855aaad90ba58960f9e20578383765e
Ubuntu <tester@techpearl.com>
2022-10-10 15:55:53 +0530

    show sketch image on hover

--- src/pages/sketch-list/sketch-list.js
+++ src/pages/sketch-list/sketch-list.js
@@ -5,7 +5,16 @@ function SketchList({sketches}) {
     return (
         <ul className="user-sketch-list">
             {
-                sketches.map(sketch => <li key={sketch.id} className="sketch-item">{sketch.name}</li>)
+                sketches.map(sketch => {
+                    return (
+                        <li key={sketch.id} className="sketch-item">
+                            {sketch.name}
+                            <span className="sketch-span">
+                                {sketch.imgData.length > 0 && <img className="sketch-img" src={sketch.imgData} />}
+                            </span>
+                        </li>
+                    )
+                })
             }
         </ul>
     );
